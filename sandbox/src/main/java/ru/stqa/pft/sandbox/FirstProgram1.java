34cdcdede2c1e4880b08d845c4ebbebedc2768fd
ildarity <riyanov_ildar@mail.ru>
2022-04-11 16:40:45 +0500

    Добавлено неск. функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram1.java
@@ -3,10 +3,25 @@ package ru.stqa.pft.sandbox;
 public class FirstProgram1 {
 
  public static void main (String[] args) {
-   System.out.println("Hello, world!");
-   int x =50;
-   int d = x * x;
-   System.out.println("Площадь квадрата со стороной" + x + " = " + d);
+   hello ("world");
+   hello ("Ildar");
+
+   double l = 5;
+   System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+   double a = 5;
+   double b = 9;
+   System.out.println("Площадь квадрата со стороной " + a + " и " + b + " = " + area(a, b));
+   }
+
+ public static void hello(String somebody) {
+   System.out.println("Hello, " + somebody + "!");
  }
 
+ public static double area (double len) {
+   return len * len;
+ }
+ public static double area (double a, double b) {
+   return a * b;
+ }
 }
