5b46d87f716ebd0bf247456d87f893e2d5578acd
ildarity <riyanov_ildar@mail.ru>
2022-04-11 16:13:56 +0500

    значения и переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/FirstProgram1.java
@@ -4,6 +4,9 @@ public class FirstProgram1 {
 
  public static void main (String[] args) {
    System.out.println("Hello, world!");
+   int x =50;
+   int d = x * x;
+   System.out.println("Площадь квадрата со стороной" + x + " = " + d);
  }
 
 }
